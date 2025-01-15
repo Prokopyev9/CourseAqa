b22cc504232b7ddf4e954d5d73826f8502ccf039
tihon.prokopev <tihon.prokopev@irlix.ru>
2025-01-15 23:50:56 +0400

    Урок по созданию и использованию параметром в тестах

--- src/test/java/tests/SimpleTests.java
+++ src/test/java/tests/SimpleTests.java
@@ -1,16 +1,32 @@
-package tests;
+ package tests;
 
+import models.People;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
-public class SimpleTests {
+import java.util.stream.Stream;
 
-    @Test
-    public void testsTwoLessThree(){
-    int a = 2;
-    int b = 3;
-        Assertions.assertTrue(a < b, "Число " + b + " больше, чем число " + a);
+ public class SimpleTests {
+
+     private static Stream <Arguments> testPeople(){
+         return Stream.of(
+                 Arguments.of(new People("Stas",18,"male")),
+                 Arguments.of(new People("Stas",18,"male")),
+                 Arguments.of(new People("Stas",18,"male"))
+                 );
+     }
+    @ParameterizedTest
+    @MethodSource("testPeople")
+    public void LessonParam(People people){
+        System.out.println(people.getName() +" " + people.getAge() + " " + people.getSex());
+        Assertions.assertTrue(people.getName().contains("s"));
+
     }
     @Test
     @DisplayName("тест суммы 2+3")
